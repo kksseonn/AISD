69f839a64ef7b233b44ac843c645bb56becebe42
kksseonn <kote13241@gmail.com>
2023-11-05 03:02:01 +0400

    added Cmake

---
+++ Lab1-var5/src/Polyline.cc
@@ -0,0 +1,6 @@
+#pragma warning(disable:6386)
+#include <Polyline/Polyline.h>
+#include <iostream>
+#include <complex>
+#include <stdexcept>
+#include <random>
---
+++ Lab1-var5/tests/Polyline_tests.cc
@@ -0,0 +1,2 @@
+#include <gtest/gtest.h>
+#include <Polyline/Polyline.h>
