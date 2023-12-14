da3bf1f2dbe1d29021a9a45261286c10ace90ba4
kksseonn <kote13241@gmail.com>
2023-12-14 16:40:37 +0400

    added comb sort

--- Lab3-var16-120/Sorting/Sorting.cpp
+++ Lab3-var16-120/Sorting/Sorting.cpp
@@ -30,6 +30,35 @@ stats insertion_sort(std::vector<int>& arr) {
     return result;
 }
 
+stats comb_sort(std::vector<int>& arr) {
+    stats result;
+    size_t n = arr.size();
+    bool swapped = true;
+    size_t gap = n;
+
+    while (gap > 1 || swapped) {
+        gap = (gap * 10) / 13;
+        if (gap < 1) {
+            gap = 1;
+        }
+
+        swapped = false;
+
+        for (size_t i = 0; i < n - gap; ++i) {
+            size_t j = i + gap;
+            ++result.comparison_count;
+
+            if (arr[i] > arr[j]) {
+                std::swap(arr[i], arr[j]);
+                swapped = true;
+                ++result.copy_count;
+            }
+        }
+    }
+
+    return result;
+}
+
 std::ostream& operator<<(std::ostream& os, const std::vector<int>& arr) {
     os << "[";
     for (size_t i = 0; i < arr.size(); ++i) {
@@ -51,7 +80,15 @@ int main() {
         std::cout << "Insertion Sort: ";
         std::cout << arr << "\n";
         std::cout << "Comparison count: " << result_insertion.comparison_count << "\n";
-        std::cout << "Copy count: " << result_insertion.copy_count << "\n";
+        std::cout << "Copy count: " << result_insertion.copy_count << "\n\n";
+
+        std::vector<int> arr2 = { 5, 2, 9, 1, 5, 6 };
+       
+        stats result_comb = comb_sort(arr2);
+        std::cout << "Comb Sort: ";
+        std::cout << arr2 << "\n";
+        std::cout << "Comparison count: " << result_comb.comparison_count << "\n";
+        std::cout << "Copy count: " << result_comb.copy_count << "\n";
     }
     catch (const std::out_of_range& e) {
         std::cerr << "Error: " << e.what() << "\n";
