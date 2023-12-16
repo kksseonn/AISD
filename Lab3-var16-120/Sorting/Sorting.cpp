7b84824de1ec74d2b9756243dea808423d69e5ac
kksseonn <kote13241@gmail.com>
2023-12-16 17:26:25 +0400

    added quick sort

--- Lab3-var16-120/Sorting/Sorting.cpp
+++ Lab3-var16-120/Sorting/Sorting.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <vector>
 #include <stdexcept>
+#include <stack>
 
 struct stats {
     size_t comparison_count = 0;
@@ -59,6 +60,53 @@ stats comb_sort(std::vector<int>& arr) {
     return result;
 }
 
+size_t partition(std::vector<int>& arr, size_t low, size_t high, stats& s) {
+    int pivot = arr[high];
+    size_t i = low;
+
+    for (size_t j = low; j < high; ++j) {
+        ++s.comparison_count;
+        if (arr[j] <= pivot) {
+            std::swap(arr[i], arr[j]);
+            ++s.copy_count;
+            ++i;
+        }
+    }
+
+    std::swap(arr[i], arr[high]);
+    ++s.copy_count;
+
+    return i;
+}
+
+void quicksort(std::vector<int>& arr, size_t low, size_t high, stats& s) {
+    std::stack<std::pair<size_t, size_t>> stack;
+    stack.push(std::make_pair(low, high));
+
+    while (!stack.empty()) {
+        auto top = stack.top();
+        stack.pop();
+
+        size_t l = top.first;
+        size_t h = top.second;
+
+        if (l < h) {
+            size_t pivotIndex = partition(arr, l, h, s);
+
+            if (pivotIndex > 0) {
+                stack.push(std::make_pair(l, pivotIndex - 1));
+            }
+            stack.push(std::make_pair(pivotIndex + 1, h));
+        }
+    }
+}
+
+stats quick_sort(std::vector<int>& arr) {
+    stats s;
+    quicksort(arr, 0, arr.size() - 1, s);
+    return s;
+}
+
 std::ostream& operator<<(std::ostream& os, const std::vector<int>& arr) {
     os << "[";
     for (size_t i = 0; i < arr.size(); ++i) {
@@ -88,7 +136,15 @@ int main() {
         std::cout << "Comb Sort: ";
         std::cout << arr2 << "\n";
         std::cout << "Comparison count: " << result_comb.comparison_count << "\n";
-        std::cout << "Copy count: " << result_comb.copy_count << "\n";
+        std::cout << "Copy count: " << result_comb.copy_count << "\n\n";
+
+        std::vector<int> arr3 = { 5, 2, 9, 1, 5, 6 };
+
+        stats result_quick = quick_sort(arr3);
+        std::cout << "Quick Sort: ";
+        std::cout << arr3 << "\n";
+        std::cout << "Comparison count: " << result_quick.comparison_count << "\n";
+        std::cout << "Copy count: " << result_quick.copy_count << "\n\n";
     }
     catch (const std::out_of_range& e) {
         std::cerr << "Error: " << e.what() << "\n";
